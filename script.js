741d8ff1b2f052a3d7909f2cff427579eb0ccf83
LoveD325 <1376460821@qq.com>
2025-04-06 14:04:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,19 @@ let clickCount = 0; // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-  "？你认真的吗…",
-  "要不再想想？",
-  "不许选这个！ ",
-  "我会很伤心…",
-  "不行:(",
+  "是不是点错了？o(╥﹏╥)o",
+  "再考虑考虑？",
+  "我觉得他可以姐",
+  "我呱呱也来为他求求情！",
+  "我小白龙来为他求求情！",
+  "我做人质，你答应他吧！",
+  "我能做饭的~",
+  "也能洗衣服~",
+  "工资不多~也会上交~",
+  "看来还是得使出我的杀手锏~",
+  "屏幕是不是不够大了？",
+  "拒绝不了我了吧？",
+  "还能点到？",
 ];
 
 // No 按钮点击事件
@@ -43,17 +51,25 @@ noButton.addEventListener("click", function () {
   mainImage.style.transform = `translateY(-${moveUp}px)`;
   questionText.style.transform = `translateY(-${moveUp}px)`;
 
-  // No 文案变化（前 5 次变化）
-  if (clickCount <= 5) {
+  // No 文案变化（前 13 次变化）
+  if (clickCount <= 13) {
     noButton.innerText = noTexts[clickCount - 1];
   }
 
-  // 图片变化（前 5 次变化）
+  // 图片变化（前 13 次变化）
   if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
   if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
   if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
   if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
-  if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
+  if (clickCount === 5) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 6) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 7) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 8) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 9) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 10) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 11) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount === 12) mainImage.src = "images/crying.png"; // 哭
+  if (clickCount >= 13) mainImage.src = "images/crying.png"; // 之后一直是哭
 });
 
 // Yes 按钮点击后，进入表白成功页面
